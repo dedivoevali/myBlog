d08871badbdb84670089225166b9220aa494d707
Alexandru Chioroglo <99559948+chioroglo@users.noreply.github.com>
2024-11-14 01:06:50 +0200

    Graph Posts Backend (#34)
    * Graph Posts Backend
    
    * Units

--- Data/Repositories/Abstract/IPostRepository.cs
+++ Data/Repositories/Abstract/IPostRepository.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Repositories.Abstract.Base;
+﻿using Common.Models.Statistics;
+using DAL.Repositories.Abstract.Base;
 using Domain;
 
 namespace DAL.Repositories.Abstract
@@ -6,5 +7,10 @@ namespace DAL.Repositories.Abstract
     public interface IPostRepository : IBaseRepository<Post>
     {
         Task<Post?> GetByTitleAsync(string title, CancellationToken cancellationToken);
+        Task<PostActivityModel> GetPostActivity(
+            int postId,
+            IEnumerable<DateTime> dates,
+            TimeMeasure measure,
+            CancellationToken ct);
     }
 }
---
+++ Service/Abstract/Statistics/IContentStatisticsService.cs
@@ -0,0 +1,12 @@
+﻿using Common.Dto.Statistics;
+using Common.Models.Statistics;
+
+namespace Service.Abstract.Statistics;
+
+public interface IContentStatisticsService
+{
+    Task<PostActivityModel> GetForPost(
+        GetStatsForPostDto dto,
+        TimeMeasure measure,
+        CancellationToken ct = default);
+}
