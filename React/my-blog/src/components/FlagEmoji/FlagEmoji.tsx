56984e3fd49cb9c676a5ae3c32a5ea3c43c28ac4
chioroglo <kioroglomobster@gmail.com>
2024-11-19 19:23:40 +0200

    indentation

--- React/my-blog/src/components/FlagEmoji/FlagEmoji.tsx
+++ React/my-blog/src/components/FlagEmoji/FlagEmoji.tsx
@@ -2,4 +2,10 @@ import "../FlagEmoji/FlagEmoji.scss";
 import { FlagEmojiProps } from "./flag-emoji-props";
 
 export const FlagEmoji = ({ emoji, fontSizePx = 12 }: FlagEmojiProps) =>
-(<div style={{ fontSize: `${fontSizePx}px` }} className="flag-emoji-content">{emoji || '🌎'}</div>)
+{
+    return (
+        <div style={{ fontSize: `${fontSizePx}px` }} className="flag-emoji-content">
+            {emoji || ' '}
+        </div>
+    )
+}
