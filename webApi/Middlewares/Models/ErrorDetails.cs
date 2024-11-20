558df34177b17dc5398bb6c44a5993aadbb68898
chioroglo <kioroglomobster@gmail.com>
2024-11-21 00:59:00 +0200

    +stacktrace

--- webApi/Middlewares/Models/ErrorDetails.cs
+++ webApi/Middlewares/Models/ErrorDetails.cs
@@ -1,19 +1,20 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace API.Middlewares.Models
 {
     public class ErrorDetails
     {
-        public ErrorDetails(int statusCode, string message)
+        public ErrorDetails(int statusCode, string message, string stackTrace)
         {
             StatusCode = statusCode;
             Message = message;
+            StackTrace = stackTrace;
         }
 
         public int StatusCode { get; set; }
         public string Message { get; set; }
-
+        public string StackTrace { get; set; }
+        
         public override string ToString()
         {
             return JsonSerializer.Serialize(this);
