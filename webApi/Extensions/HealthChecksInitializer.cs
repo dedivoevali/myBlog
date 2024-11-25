4258de605d5fa025a42c76192d61a2f29da17fc2
chioroglo <kioroglomobster@gmail.com>
2024-11-26 01:47:57 +0200

    health check azure blob

--- webApi/Extensions/HealthChecksInitializer.cs
+++ webApi/Extensions/HealthChecksInitializer.cs
@@ -1,16 +1,27 @@
-﻿namespace API.Extensions;
+﻿using Azure.Storage.Blobs;
+using Common.Options;
+using Microsoft.Extensions.Options;
+
+namespace API.Extensions;
 
 public static class HealthChecksInitializer
 {
     public static IServiceCollection AddMyHealthChecks(this IServiceCollection services, IConfiguration configuration)
     {
         var sql = configuration.GetConnectionString("Blog");
+        var timeout = TimeSpan.FromSeconds(3);
 
         services.AddHealthChecks()
             .AddSqlServer(
                 sql!,
                 healthQuery: "SELECT 1 FROM dbo.[User]",
-                timeout: TimeSpan.FromSeconds(3));
+                timeout: timeout)
+            .AddAzureBlobStorage(clientFactory: (provider) =>
+            {
+                var options = provider.GetService<IOptions<AzureStorageCredentialOptions>>().Value;
+                return new BlobServiceClient(options.ConnectionString);
+            }, timeout: timeout);
+
 
         return services;
     }
