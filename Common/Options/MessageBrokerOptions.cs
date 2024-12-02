2a7cf1e26f3333576183f411dd44d76a697e4973
Alexandr Chioroglo <kioroglomobster@gmail.com>
2024-12-02 12:29:47 +0200

    MessageBrokerOptions

---
+++ Common/Options/MessageBrokerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.Options;
+
+public class MessageBrokerOptions : BaseApplicationOptions
+{
+    public new static string Config => "MessageBroker";
+    public MessageBrokerProvider Provider { get; set; }
+}
+
+public enum MessageBrokerProvider
+{
+    InMemory,
+    Rabbit
+}
