6551b64a13d6813250aaa7fe1e6a0fdeeae8dc6a
Alexandr Chioroglo <kioroglomobster@gmail.com>
2024-12-02 12:52:52 +0200

    JwtBearerEvents

--- webApi/Extensions/Auth/JwtBearerConfiguration.cs
+++ webApi/Extensions/Auth/JwtBearerConfiguration.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.Net;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Text.Json;
+using API.Middlewares.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace API.Extensions.Auth
 {
@@ -16,12 +20,29 @@ namespace API.Extensions.Auth
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuer = true,
-                        ValidateAudience = false,
+                        ValidateAudience = true,
                         ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
                         ValidIssuer = configuration["Jwt:Issuer"],
                         ValidAudience = configuration["Jwt:Audience"],
                         IssuerSigningKey = new SymmetricSecurityKey(securityKeyBytes)
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnChallenge = async context =>
+                        {
+                            context.HandleResponse();
+
+                            var error = new ErrorDetails(
+                                statusCode: (int)HttpStatusCode.Unauthorized,
+                                message: "Authentication failed!",
+                                stackTrace: string.Empty
+                            );
+
+                            var json = JsonSerializer.SerializeToUtf8Bytes(error);
+                            await context.Response.BodyWriter.WriteAsync(json);
+                        }
                     };
                 });
         }
