1571eee7166bf3e291b055d54307104457c7f6b6
Alexandr Chioroglo <kioroglomobster@gmail.com>
2025-01-07 14:51:51 +0200

    mapping dtos for Authentication

---
+++ Common/MappingProfiles/AuthProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Common.Dto.Auth;
+using Common.Models;
+
+namespace Common.MappingProfiles;
+
+public class AuthProfile : Profile
+{
+    public AuthProfile()
+    {
+        CreateMap<AuthorizationResponse, AuthorizationResponseModel>();
+    }
+}
---
+++ Common/Models/AuthorizationResponseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Common.Models;
+
+public class AuthorizationResponseModel
+{
+    public string AccessToken { get; set; }
+}
