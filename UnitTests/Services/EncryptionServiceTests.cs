0cf56dcddbf8d099169ca76e4433a1bfe507b334
Alexandru Chioroglo <99559948+chioroglo@users.noreply.github.com>
2025-01-08 22:14:13 +0200

    test (#74)

--- UnitTests/Services/EncryptionServiceTests.cs
+++ UnitTests/Services/EncryptionServiceTests.cs
@@ -1,6 +1,6 @@
-﻿using Common.Options;
+﻿using System.Runtime.InteropServices;
+using Common.Options;
 using Common.Validation;
-using DAL.Repositories.Abstract;
 using Microsoft.Extensions.Options;
 using Service.Auth;
 
@@ -35,5 +35,19 @@ public class EncryptionServiceTests
 
         // Assert
         response.Value.Length.Should().Be(expectedLength);
+    }
+
+    [Theory]
+    [InlineData("password", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8")]
+    [InlineData("lovemama123", "9bcd53793c43361386708990a5a7827140deb591910da5fd8649a9b81759ffa6")]
+    public async Task EncryptPassword_ShouldEncrypt_Sha256(string input, string expected)
+    {
+        // Arrange
+
+        // Act
+        var actual = _subject.EncryptPassword(input);
+
+        // Assert
+        actual.Should().Be(expected);
     }
 }
