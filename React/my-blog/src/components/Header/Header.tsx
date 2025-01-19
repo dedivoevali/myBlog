5c3ff9e517251bc29fa74b8e6bd6eae2dc1993ac
Alexandr Chioroglo <kioroglomobster@gmail.com>
2025-01-19 23:08:59 +0200

    reafctor

--- React/my-blog/src/components/Header/Header.tsx
+++ React/my-blog/src/components/Header/Header.tsx
@@ -1,33 +1,15 @@
-import { Avatar, Box, Button, Toolbar, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { Box, Button, Toolbar } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { ApplicationState, CurrentUserState } from '../../redux';
 import { CustomNavbar } from '../CustomNavbar';
 import { CustomNavLink } from '../CustomNavLink';
 import { Link } from 'react-router-dom';
-import { AccountMenuDropdown } from '../AccountMenuDropdown';
 import styles from "./header.module.scss";
-import { UserApi } from '../../shared/api/http/user-api';
-import { UserBadgeModel } from '../../shared/api/types/user';
+import { HeaderUserInfoBar } from './HeaderUserInfoBar';
 
 const Header = () => {
     const user = useSelector<ApplicationState, (CurrentUserState | undefined | null)>(state => state.user);
-    const [userInfo, setUserInfo] = useState<UserBadgeModel>();
-    const [avatarUrl, setAvatarUrl] = useState<string>();
-
-    useEffect(() => {
-        setUserInfo(undefined);
-        setAvatarUrl(undefined);
-        if (user) {
-            UserApi.getCurrentUserBadge().then((response) => {
-                setUserInfo(response.data);
-                UserApi.getAvatarUrlById(user.id).then((response1) => {
-                    const avatarUrl = response1.data;
-                    setAvatarUrl(avatarUrl);
-                });
-            });
-        }
-    }, [user]);
+    console.log(user);
 
     return (
         <CustomNavbar>
@@ -38,14 +20,7 @@ const Header = () => {
                     </CustomNavLink>
                 </Box>
                 {
-                    user ?
-                        <div>
-                            <Typography display={"inline"}>Welcome, {userInfo?.username}!</Typography>
-                            <AccountMenuDropdown icon={<Avatar
-                                src={avatarUrl}>{userInfo?.initials}</Avatar>}/>
-                        </div>
-                        :
-                        <Button variant="contained" component={Link} to="/login">Login</Button>
+                    user ? <HeaderUserInfoBar user={user}/> : <Button variant="contained" component={Link} to="/login">Login</Button>
                 }
             </Toolbar>
         </CustomNavbar>
